08304f3007d45dcef4bad34d9eafdd9e8476b472
luizolivas <luizolivas@gmail.com>
2025-05-09 12:07:43 -0300

    Resolve exercicio 1

---
+++ Exercicio/Exercicio1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteTecnicoTargetErp.Exercicio
+{
+    internal class Exercicio1
+    {
+        public static void Executar()
+        {
+            int indice = 13;
+            int soma = 0;
+            int k = 0;
+
+            while(k < indice)
+            {
+                k = k + 1;
+                soma = soma + k;
+            }
+            Console.WriteLine($"Valor soma: {soma}");
+            Console.ReadLine();
+        }
+    }
+}
